f31bb5d99f7d45ec61cada0f32499108ff040931
Zoffix Znet <cpan@zoffix.com>
2014-01-25 22:38:45 -0500

    Fixed up the example

--- examples/lipsum.pl
+++ examples/lipsum.pl
@@ -7,7 +7,7 @@ use lib qw{../lib lib};
 use WWW::Lipsum;
 
 my $lipsum = WWW::Lipsum->new(
-    html => 1, amount => 50, what => 'bytes', start => 0
+    html => 0, amount => 5, what => 'paras', start => 0
 );
     
 print "$lipsum\n";
